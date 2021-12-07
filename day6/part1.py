1af6b4ed4e5d7db255191ca949412f1e2d109829
Marcus Butler <mtbutler07@gmail.com>
2021-12-07 12:11:06 -0500

    day 6 part 1

---
+++ day6/part1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3.9
+from typing import List
+
+
+def simulate(swarm: List) -> List[int]:
+    results = []
+
+    for fish in swarm:
+        if fish == 0:
+            results.append(6)
+            results.append(8)
+        else:
+            results.append(fish - 1)
+    return results
+
+
+def load_input(filepath: str) -> List[List[int]]:
+    with open(filepath, "r") as f:
+        for line in f.readlines():
+            line = line.strip().split(",")
+            results = [int(i) for i in line]
+    return results
+
+
+if __name__ == "__main__":
+    current = load_input("day6/sample.txt")
+    # print(current)
+    for i in range(80):
+        current = simulate(current)
+        print(f"length of list {len(current)}")
